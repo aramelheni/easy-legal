2adb8f91749e93c13615bd2f38211843c1b97f12
ahmed khemiri <ahmedkhemiri466@yahoo.com>
2023-11-20 10:27:48 +0100

    ignore this commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import { useEffect } from 'react';
 import './App.css';
-import { httpGet, httpPost } from './Connection';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-        crossorigin="anonymous"
-      />
+      
     </>
   );
 }
