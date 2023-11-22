44ede2e85812281dd6b8f5b5c6d1ac3b49add021
ahmed khemiri <ahmedkhemiri466@yahoo.com>
2023-11-22 08:12:59 +0100

    fixated the account panel position

--- src/components/navigation_bar/NavigationBar.js
+++ src/components/navigation_bar/NavigationBar.js
@@ -3,18 +3,22 @@ import NavigationButton from "./button/NavigationButton";
 import { navigationSettings } from "../../Navigation";
 
 export default function NavigationBar() {
-    return (<div id="navigation-bar">
-        <h1 id="logo-text">Easy Legal</h1>
-        <hr className="horizontal-line" />
-        <div id="buttons">
-            {
-                navigationSettings.map((setting) => (
-                    <NavigationButton navigationSetting={setting} />
-                ))
-            }
-        </div>
-        <div id="account-panel">
-            
-        </div>
-    </div >);
+    return (
+        <div id="navigation-bar">
+            <h1 id="logo-text">Easy Legal</h1>
+            <hr className="horizontal-line" />
+            <div id="content">
+                <div id="buttons">
+                    {
+                        navigationSettings.map((setting) => (
+                            <NavigationButton navigationSetting={setting} />
+                        ))
+                    }
+                </div>
+                <div id="account-panel">
+                    
+                </div>
+            </div>
+        </div >
+    );
 }
