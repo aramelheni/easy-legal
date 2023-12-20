0f90a3b1a2d8b9b936d567cf1097f2c2f0bf02d8
ahmed khemiri <ahmedkhemiri466@yahoo.com>
2023-12-21 00:47:50 +0100

    calendar

--- src/components/calendar/task_preview/TaskPreview.js
+++ src/components/calendar/task_preview/TaskPreview.js
@@ -1,8 +1,12 @@
 import Modal from '../../modal/Modal.js';
+import AppPaper from '../../utilities/app_paper/AppPaper.js';
 
 export default function TaskPreview({onClose}) {
     return (<div>
         <Modal onClose={onClose}>
+            <AppPaper>
+                <p>hi</p>
+            </AppPaper>
         </Modal>
     </div>);
 }
